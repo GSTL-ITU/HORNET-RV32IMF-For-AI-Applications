dd7f6fb94b2308cf297eed297b9e8ab1e1557d26
Yusuf Tekin <josephburak@hotmail.com>
2026-05-14 13:51:54 +0300

    Simplified top module for faster clock cycles, better routing and more readable code.

--- v_1.2_accelerator/src/constrs_1/imports/constraints/Nexys-4-DDR-Master.xdc
+++ v_1.2_accelerator/src/constrs_1/imports/constraints/Nexys-4-DDR-Master.xdc
@@ -30,10 +30,10 @@ create_clock -add -name sys_clk_pin -period 10.00 -waveform {0 5} [get_ports {M1
 
 ## LEDs
 
-set_property -dict { PACKAGE_PIN H17   IOSTANDARD LVCMOS33 } [get_ports { led0 }]; #IO_L18P_T2_A24_15 Sch=led[0]
-set_property -dict { PACKAGE_PIN K15   IOSTANDARD LVCMOS33 } [get_ports { led1 }]; #IO_L24P_T3_RS1_15 Sch=led[1]
+#set_property -dict { PACKAGE_PIN H17   IOSTANDARD LVCMOS33 } [get_ports { led0 }]; #IO_L18P_T2_A24_15 Sch=led[0]
+#set_property -dict { PACKAGE_PIN K15   IOSTANDARD LVCMOS33 } [get_ports { led1 }]; #IO_L24P_T3_RS1_15 Sch=led[1]
 #set_property -dict { PACKAGE_PIN J13   IOSTANDARD LVCMOS33 } [get_ports { LED[2] }]; #IO_L17N_T2_A25_15 Sch=led[2]
-set_property -dict { PACKAGE_PIN N14   IOSTANDARD LVCMOS33 } [get_ports { led3 }]; #IO_L8P_T1_D11_14 Sch=led[3]
+#set_property -dict { PACKAGE_PIN N14   IOSTANDARD LVCMOS33 } [get_ports { led3 }]; #IO_L8P_T1_D11_14 Sch=led[3]
 #set_property -dict { PACKAGE_PIN R18   IOSTANDARD LVCMOS33 } [get_ports { LED[4] }]; #IO_L7P_T1_D09_14 Sch=led[4]
 #set_property -dict { PACKAGE_PIN V17   IOSTANDARD LVCMOS33 } [get_ports { LED[5] }]; #IO_L18N_T2_A11_D27_14 Sch=led[5]
 #set_property -dict { PACKAGE_PIN U17   IOSTANDARD LVCMOS33 } [get_ports { LED[6] }]; #IO_L17P_T2_A14_D30_14 Sch=led[6]
@@ -45,7 +45,7 @@ set_property -dict { PACKAGE_PIN N14   IOSTANDARD LVCMOS33 } [get_ports { led3 }
 #set_property -dict { PACKAGE_PIN V15   IOSTANDARD LVCMOS33 } [get_ports { LED[12] }]; #IO_L16P_T2_CSI_B_14 Sch=led[12]
 #set_property -dict { PACKAGE_PIN V14   IOSTANDARD LVCMOS33 } [get_ports { LED[13] }]; #IO_L22N_T3_A04_D20_14 Sch=led[13]
 #set_property -dict { PACKAGE_PIN V12   IOSTANDARD LVCMOS33 } [get_ports { LED[14] }]; #IO_L20N_T3_A07_D23_14 Sch=led[14]
-set_property -dict { PACKAGE_PIN V11   IOSTANDARD LVCMOS33 } [get_ports { gpio_trigger_o }]; #IO_L21N_T3_DQS_A06_D22_14 Sch=led[15]
+#set_property -dict { PACKAGE_PIN V11   IOSTANDARD LVCMOS33 } [get_ports { gpio_trigger_o }]; #IO_L21N_T3_DQS_A06_D22_14 Sch=led[15]
 
 #set_property -dict { PACKAGE_PIN R12   IOSTANDARD LVCMOS33 } [get_ports { LED16_B }]; #IO_L5P_T0_D06_14 Sch=led16_b
 #set_property -dict { PACKAGE_PIN M16   IOSTANDARD LVCMOS33 } [get_ports { LED16_G }]; #IO_L10P_T1_D14_14 Sch=led16_g
@@ -94,7 +94,7 @@ set_property -dict { PACKAGE_PIN C12   IOSTANDARD LVCMOS33 } [get_ports { rst_ni
 ##Pmod Header JA
 
 #set_property -dict { PACKAGE_PIN C17   IOSTANDARD LVCMOS33 } [get_ports { gpio_trigger_o }]; #IO_L20N_T3_A19_15 Sch=ja[1]
-set_property -dict { PACKAGE_PIN D18   IOSTANDARD LVCMOS33 } [get_ports { uart_debug_stb }]; #IO_L21N_T3_DQS_A18_15 Sch=ja[2]
+#set_property -dict { PACKAGE_PIN D18   IOSTANDARD LVCMOS33 } [get_ports { uart_debug_stb }]; #IO_L21N_T3_DQS_A18_15 Sch=ja[2]
 #set_property -dict { PACKAGE_PIN E18   IOSTANDARD LVCMOS33 } [get_ports { led1 }]; #IO_L21P_T3_DQS_15 Sch=ja[3]
 #set_property -dict { PACKAGE_PIN G17   IOSTANDARD LVCMOS33 } [get_ports { led2 }]; #IO_L18N_T2_A23_15 Sch=ja[4]
 #set_property -dict { PACKAGE_PIN D17   IOSTANDARD LVCMOS33 } [get_ports { led4 }]; #IO_L16N_T2_A27_15 Sch=ja[7]
--- v_1.2_accelerator/src/sources_1/hornet/fpga_top.v
+++ v_1.2_accelerator/src/sources_1/hornet/fpga_top.v
@@ -4,19 +4,14 @@ module fpga_top (
     input  wire M100_clk_i,
     input  wire rst_ni,
     input  wire rx_i,
-    output wire tx_o,
-    output wire uart_debug_stb,
-    output wire led0,
-    output wire led1,
-    output wire led3,
-    output wire gpio_trigger_o
+    output wire tx_o
 );
 
     // -------------------------------------------------------------------------
     // Parameters
     // -------------------------------------------------------------------------
-    parameter SYS_CLK_FREQ = 20000000;
-    parameter NUM_SLAVES   = 7;
+    parameter SYS_CLK_FREQ = 25000000;
+    parameter NUM_SLAVES   = 5;
     parameter MEMORY_INIT  = "memory_init.mem";
     parameter RAM_DEPTH    = 4096;
 
@@ -24,15 +19,12 @@ module fpga_top (
     // Internal Signals
     // -------------------------------------------------------------------------
     wire tx_o_tmp;
-    wire loader_reset;
-    wire reset;
     wire irq_ack_o;
     wire mtip;
     wire rx_irq_o;
     wire [7:0] rx_byte;
 
     assign tx_o = tx_o_tmp;
-    assign reset = loader_reset & rst_ni;
 
     // Clocking
     wire clk_i, locked;
@@ -105,18 +97,10 @@ module fpga_top (
     // Slave 3: UART
     assign slave_adr_begin[3] = 32'h1000_8010;
     assign slave_adr_end[3]   = 32'h1000_8013;
-
-    // Slave 4: Loader
-    assign slave_adr_begin[4] = 32'h1000_8014;
-    assign slave_adr_end[4]   = 32'h1000_8014;
-
-    // Slave 5: GPIO
-    assign slave_adr_begin[5] = 32'h1000_8020;
-    assign slave_adr_end[5]   = 32'h1000_8023;
     
-    // Slave 6: AI Accelerator
-    assign slave_adr_begin[6] = 32'h1000_9000;
-    assign slave_adr_end[6]   = 32'h1000_9224;
+    // Slave 4: AI Accelerator
+    assign slave_adr_begin[4] = 32'h1000_9000;
+    assign slave_adr_end[4]   = 32'h1000_9224;
 
     // -------------------------------------------------------------------------
     // Crossbar / Interconnect Logic
@@ -129,7 +113,7 @@ module fpga_top (
     assign wb_adr_i[0]   = inst_wb_adr_o;
     assign wb_dat_i[0]   = inst_wb_dat_o;
     assign wb_sel_i[0]   = inst_wb_sel_o;
-    assign wb_rst_i[0]   = ~reset;
+    assign wb_rst_i[0]   = ~rst_ni;
     assign wb_clk_i[0]   = clk_i;
     
     assign inst_wb_dat_i = wb_dat_o[0];
@@ -154,7 +138,7 @@ module fpga_top (
             if (i == 4) begin
                 assign wb_rst_i[i] = ~rst_ni;
             end else begin
-                assign wb_rst_i[i] = ~reset;
+                assign wb_rst_i[i] = ~rst_ni;
             end
         end
     endgenerate
@@ -201,7 +185,7 @@ module fpga_top (
     assign data_wb_stall_i = r_data_wb_stall_i;
     assign data_wb_err_i   = r_data_wb_err_i;
     assign data_wb_clk_i   = clk_i;
-    assign data_wb_rst_i   = ~reset;
+    assign data_wb_rst_i   = ~rst_ni;
 
     // -------------------------------------------------------------------------
     // Module Instantiations
@@ -211,7 +195,7 @@ module fpga_top (
         .reset_vector(32'h0)
     ) core0 (
         .clk_i(clk_i),
-        .rst_ni(reset), // active-low reset
+        .rst_ni(rst_ni), // active-low reset
 
         // Wishbone interface for data memory
         .data_wb_cyc_o(data_wb_cyc_o),
@@ -245,7 +229,7 @@ module fpga_top (
         .irq_ack_o(irq_ack_o)
     );
 
-    (* DONT_TOUCH = "yes" *) memory_2rw_wb #(
+    memory_2rw_wb #(
         .RAM_DEPTH(RAM_DEPTH), 
         .MEMORY_INIT(MEMORY_INIT)
     ) memory_inst (
@@ -317,10 +301,8 @@ module fpga_top (
         .rx_byte_o(rx_byte),
         .rx_irq_o(rx_irq_o)
     );
-
-    loader_wb #(
-        .SYS_CLK_FREQ(SYS_CLK_FREQ)
-    ) loader0_inst (
+    
+    wb_accelerator_wrapper acc_inst (
         .wb_cyc_i(wb_cyc_i[4]),
         .wb_stb_i(wb_stb_i[4]),
         .wb_we_i(wb_we_i[4]),
@@ -332,44 +314,6 @@ module fpga_top (
         .wb_dat_o(wb_dat_o[4]),
         .wb_err_o(wb_err_o[4]),
         .wb_rst_i(wb_rst_i[4]),
-        .wb_clk_i(wb_clk_i[4]),
-
-        .uart_rx_irq(rx_irq_o),
-        .uart_rx_byte(rx_byte),
-        .reset_o(loader_reset),
-        .led0(led0), 
-        .led1(led1), 
-        .led3(led3)
-    );
-
-    gpio_wb gpio_inst (
-        .wb_cyc_i(wb_cyc_i[5]),
-        .wb_stb_i(wb_stb_i[5]),
-        .wb_we_i(wb_we_i[5]),
-        .wb_adr_i(wb_adr_i[5]),
-        .wb_dat_i(wb_dat_i[5]),
-        .wb_sel_i(wb_sel_i[5]),
-        .wb_stall_o(wb_stall_o[5]),
-        .wb_ack_o(wb_ack_o[5]),
-        .wb_dat_o(wb_dat_o[5]),
-        .wb_err_o(wb_err_o[5]),
-        .wb_rst_i(wb_rst_i[5]),
-        .wb_clk_i(wb_clk_i[5]),
-        .trigger_o(gpio_trigger_o)
-    );
-    
-    wb_accelerator_wrapper acc_inst (
-        .wb_cyc_i(wb_cyc_i[6]),
-        .wb_stb_i(wb_stb_i[6]),
-        .wb_we_i(wb_we_i[6]),
-        .wb_adr_i(wb_adr_i[6]),
-        .wb_dat_i(wb_dat_i[6]),
-        .wb_sel_i(wb_sel_i[6]),
-        .wb_stall_o(wb_stall_o[6]),
-        .wb_ack_o(wb_ack_o[6]),
-        .wb_dat_o(wb_dat_o[6]),
-        .wb_err_o(wb_err_o[6]),
-        .wb_rst_i(wb_rst_i[6]),
-        .wb_clk_i(wb_clk_i[6])
+        .wb_clk_i(wb_clk_i[4])
     );
 endmodule
